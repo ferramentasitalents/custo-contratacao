0e20afec05e1f2741b1135117bc3f3548a0f4dff
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 16:07:28 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-
+<script>
+function calculateCost() {
+  const gastoRS = parseFloat(document.getElementById("gasto-rs").value);
+  const gastoHunting = parseFloat(document.getElementById("gasto-hunting").value);
+  const numVagas = parseFloat(document.getElementById("num-vagas").value);
+  const cost = ((gastoRS + gastoHunting) / numVagas).toFixed(2);
+  document.getElementById("result").innerHTML = `Custo de contratação por vaga: R$ ${cost}`;
+}
+</script>
